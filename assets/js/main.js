128b5c01423a53036919f9ddfd495f06bc90bd55
Tyler Ives <ct.ives@gmail.com>
2017-08-24 21:11:00 -0700

    added company logos and broke out portions of the site into includes.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,9 @@
 $( document ).ready(function(){
     $(".button-collapse").sideNav();
-})
+
+    $('.phone').text(function(i, text) {
+        return text.replace(/(\d{3})(\d{3})(\d{4})/, '$1-$2-$3');
+    });
+});
+
+
